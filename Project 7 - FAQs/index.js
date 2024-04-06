d5f97698283143972af6f2c2b344bde89cf3a7d1
HamzaLiaqat-5122 <haamzaaliaaqaat786@gmail.com>
2024-04-06 15:52:17 +0500

    Add 11 Projects

--- Project 7 - FAQs/index.js
+++ Project 7 - FAQs/index.js
@@ -1,19 +1,19 @@
 const dataArray = [
     {
         title: 'Why is JavaScript cool?',
-        detail: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto molestiae iure quam autem dignissimos vitae maiores doloremque necessitatibus mollitia nostrum deserunt et ut aliquid, temporibus itaque enim, repellendus nesciunt quasi?'
+        detail: 'JavaScript can be used in both the front-end and back-end of web development. JavaScript is standardized so it is frequently updated with new versions. JavaScript integrates easily with HTML and CSS. JavaScript allows websites to have interactivity like scroll transitions and object movement.'
     },
     {
         title: 'What is JavaScript?',
-        detail: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto molestiae iure quam autem dignissimos vitae maiores doloremque necessitatibus mollitia nostrum deserunt et ut aliquid, temporibus itaque enim, repellendus nesciunt quasi?'
+        detail: 'JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive.'
     },
     {
         title: 'How can i learn JavaScript?',
-        detail: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto molestiae iure quam autem dignissimos vitae maiores doloremque necessitatibus mollitia nostrum deserunt et ut aliquid, temporibus itaque enim, repellendus nesciunt quasi?'
+        detail: 'Watch JavaScript Tutorials.Read JavaScript Books.Practice With JavaScript Projects. Listen to JavaScript Podcasts. Join Coding Communities or Groups. Apply for a Web Developer Internship.Practice With JavaScript Flashcards.Practice in Interactive JavaScript Environments.'
     },
     {
         title: 'Where can i learn JavaScript?',
-        detail: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto molestiae iure quam autem dignissimos vitae maiores doloremque necessitatibus mollitia nostrum deserunt et ut aliquid, temporibus itaque enim, repellendus nesciunt quasi?'
+        detail: 'Udemy Free JavaScript Tutorials.Pluralsight.com.Coursera JavaScript Foundation Courses.Freecodecamp.org.Codecademy.'
     }
 ];
 
